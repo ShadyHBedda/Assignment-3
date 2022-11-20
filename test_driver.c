ca83330c1ffd8616b21d83c5ef718efd1146281b
ShadyHBedda <shadyhazem@gmail.com>
2022-11-20 20:02:18 +0200

    Created core function for count_race.c, will move it there and import it into test_driver soon

--- test_driver.c
+++ test_driver.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
-int *array;
-int length = 1000;
+
+int length = 100;
 int count;
 
-int count1s ()
+int count1s (int *arrayInput)
 {
     int i;
     count = 0;
 
     for (i=0; i<length; i++)
     {
-        if (array[i] == 1)
+        if (arrayInput[i] == 1)
         {
             count++;
         }
@@ -22,20 +23,54 @@ int count1s ()
     return count;
 }
 
+
+int race_count(int *arrayInput, int numberOfThreads) {
+    int T[numberOfThreads];
+    for (int i = 0; i < numberOfThreads; i++) {
+        T[i] = 0;
+    }
+    omp_set_num_threads(numberOfThreads);
+    int parallel_count = 0;
+    #pragma omp parallel
+    {
+        int i;
+        #pragma omp for
+        for (i=0; i<length; i++)
+        {
+            if (arrayInput[i] == 1)
+            {
+                parallel_count++;
+                T[omp_get_thread_num()]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < numberOfThreads; i++) {
+        printf("\nNumber of 1's found by Thread %d is %d", i, T[i]);
+    }
+    return parallel_count;
+};
+
+
+
 int main(void)
 {
     srand(time(0));
     int randArray[length],i;
     for(i=0;i<length;i++)
         randArray[i]=rand()%6;   //Generate number between 0 and 5
+    /*
     printf("\nElements of the array::");
     for(i=0;i<length;i++)
     {
         printf("\nElement number %d::%d",i+1,randArray[i]);
     }
+    */
 
-    array = &randArray;
-    printf("\nNumber of 1's is %d", count1s());
+    int *array = &randArray;
+    printf("\nNumber of 1's is %d", count1s(array));
+    printf("\nNumber of 1's found by race_count is %d", race_count(array, 10));
+
 }
 
 
